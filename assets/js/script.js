f4b267b844cf2ac0a040d7eb3497a95af20fdb1c
Mats Gustafsson <mats.gustafsson83@gmail.com>
2023-05-11 16:41:46 +0000

    function Login() added to the script.js, and on the quiz.html in header where i want the username to be displayed next to a Welcome text, using the localstorage setitem and getitem to display it. And the browser (alert) if no name is present.

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// login // username display
+
+function Login(){
+    var gameNameInput = document.getElementById("username");
+    var username = gameNameInput.value;
+
+    if (!username) {
+        alert("Please Choose a username");
+        return;
+    }
+
+    window.localStorage.setItem("username", username);
+    window.location.href = "quiz.html";
+}
+
+var username = window.localStorage.getItem("username");
+document.getElementById("username-displayed").textContent = username;
