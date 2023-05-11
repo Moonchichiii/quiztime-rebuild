6e36dc519546f06263660a93d7e85e50102e99b1
Mats Gustafsson <mats.gustafsson83@gmail.com>
2023-05-11 19:07:45 +0000

    copying from the old quiz, and adding shuffle and current questions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,17 +25,34 @@ function Login() {
 
 // Start button (start quiz)
 
-
 const startButton = document.getElementById("start-btn");
 startButton.addEventListener('click', startQuiz);
 
+const questionElement = document.getElementById("questions");
+const nextButton = document.getElementById("next-btn");
+nextButton.addEventListener('click', nextQuestion);
 
+let shuffleQuestion, currentQuestionIndex;
 
+const answerButtonElement = document.getElementById("answer-buttons"); 
 
-function startQuiz() {
+function startQuiz() {  
   var gameInstructions = document.getElementById("instructions");
   gameInstructions.classList.add("hide");
 
   var questionContainer = document.getElementById("question-container");  
   startButton.classList.add("hide");
+  
+  
+  
+  questionContainer.classList.remove("hide");
+
+  var scoreBoard = document.getElementById("scoreboard");
+  scoreBoard.classList.remove("hide");
+
+  nextButton.classList.remove("hide");
+
+  shuffleQuestion = questions.sort(() => Math.random() - 0.5)
+  currentQuestionIndex = 0;
+  showQuestion(shuffleQuestion[currentQuestionIndex]);
 }
